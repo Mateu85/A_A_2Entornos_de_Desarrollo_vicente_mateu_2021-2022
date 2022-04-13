e8de33495bd836490924dcea12a784a34cc753d3
vicent.mateu <vicent.mateu@onlean.es>
2022-04-13 18:23:08 +0200

    Menu

---
+++ src/main/java/com/svalero/OptionsMenu.java
@@ -0,0 +1,40 @@
+package com.svalero;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class OptionsMenu {
+
+    private Scanner Scanner;
+    private List<Task> catalogTasks;
+
+    public OptionsMenu() {
+        Scanner = new Scanner(System.in);
+        catalogTasks = new ArrayList<>();
+    }
+
+    public void mostrarMenu() {
+
+        //generateTaskList();
+        String option = null;
+
+        do {
+            System.out.println("List of asigments in your area.");
+            System.out.println("1. Add Task.");
+            System.out.println("2. Search a Task.");
+            System.out.println("3. Delete a Task.");
+            System.out.println("4. Modify a Task.");
+            System.out.println("5. Look up the list of Tasks.");
+            System.out.println("6. EXIT");
+            System.out.print("Type your Option: ");
+            option = Scanner.nextLine();
+
+            switch(option) {
+
+            }
+        } while (!option.equals("6"));
+    }
+
+
+}
---
+++ src/main/java/com/svalero/WebApp.java
@@ -0,0 +1,9 @@
+package com.svalero;
+
+public class WebApp {
+
+    public static void main(String[] args) {
+        OptionsMenu optionsMenu = new OptionsMenu();
+        optionsMenu.mostrarMenu();
+    }
+}
